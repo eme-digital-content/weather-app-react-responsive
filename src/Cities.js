2b299f3907a812ece452dba2983ae8f3c31581c5
mbellorin2 <mbellorin2@gmail.com>
2023-09-24 17:18:02 -0300

    Revert "first commit"
    This reverts commit a5f438b8aa5234c98bdd941802e34e6638201630.

--- src/Cities.js
+++
@@ -1,8 +0,0 @@
-import "./cities.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
-
-export default function Cities() {
-  return <div className="Search"></div>;
-}
